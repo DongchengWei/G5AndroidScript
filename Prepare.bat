778b1b184031f584e6ca0c0c129849d28817d486
DongchengWei <Dongcheng.Wei@desay-svautomotive.com>
2017-07-10 19:50:58 +0800

    Change delay time

--- Prepare.bat
+++ Prepare.bat
@@ -5,7 +5,6 @@ ping 127.0.0.1 -n 5 -w 1000 > nul
 adb shell reboot
 java -jar %WORKSPACE%\RebootWait.jar
 adb root
-ping 127.0.0.1 -n 5 -w 1000 > nul
 ping 127.0.0.1 -n 10 -w 1000 > nul
 adb shell rm -rf /sdcard/app_*
 adb shell rm -rf /sdcard/anr*
